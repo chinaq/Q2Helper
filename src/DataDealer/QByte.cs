d0dc844a2b7df44c8c6fc51eaf8ce7b78d372266
chinaq <aaa6263@sina.com>
2016-09-12 14:06:31 +0800

    init2

---
+++ src/DataDealer/QByte.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QHelper.QDataHelper
+{
+    public class QByte
+    {
+        public static double ToBcd2Decimal(byte[] data, int posStart, int len)
+        {
+            int posEnd = posStart + len - 1;
+            StringBuilder sb = new StringBuilder();
+            for (int i = posStart; i < posEnd; i++)
+            {
+                string s = data[i].ToString("X2");
+                sb.Append(s);
+            }
+            string lastVal = data[posEnd].ToString("X2");
+            sb.Append(".").Append(lastVal);
+            return double.Parse(sb.ToString());
+        }
+
+        public static bool CheckAccumulation(byte[] bytesToCheck, int startPos, int endPos, int checkPos)
+        {
+            int totalLen = bytesToCheck.Length;
+            int _endPos = (totalLen + endPos) % totalLen;
+            int _checkPos = (totalLen + checkPos) % totalLen;
+
+            byte checkResult = 0;
+            for (int i = startPos; i <= _endPos; i++)
+            {
+                checkResult += bytesToCheck[i];
+            }
+            return checkResult == bytesToCheck[_checkPos];
+        }
+    }
+}
---
+++ test/QHelper.UnitTest/QDataDealer/QByteTest.cs
@@ -0,0 +1,51 @@
+﻿using QHelper.QDataHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace QHelper.UnitTest.DataDealer
+{
+    public class BcdTest
+    {
+        [Fact]
+        public void Test_ToBcd2Decimal()
+        {
+            //0.arrange
+            byte[] bytes = new byte[]
+            {   0x88, 0x88,
+                    0x12, 0x34, 0x056, 0x78, 0x99,
+                    0x88
+            };
+            //1.action
+            double vol = QByte.ToBcd2Decimal(bytes, 2, 5);
+            //2.assert
+            Assert.Equal(12345678.99d, vol);
+        }
+
+
+        [Fact]
+        public void Test_CheckAccumulation() {
+            //0.arrange
+            byte[] bytesToCheck = new byte[] {
+                0x68, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x68, 0x13, 0x07, 0x44, 0x44, 0x55, 0x66, 0x77, 0x88, 0x33,
+                0xf5, //to check
+                0xff  //end
+            };
+            int startPos = 0;
+            int endPos = -3;
+            int checkPos = -2;
+
+            //1.action
+            bool isValid = QByte.CheckAccumulation(bytesToCheck, startPos, endPos, checkPos);
+
+            //2.assert
+            Assert.True(isValid);
+        }
+
+        //0.arrange
+        //1.action
+        //2.assert
+    }
+}
